ec86106444ba6606a2b9be0dfefa3cb641a0a4f0
Ryoga-exe <git@ryoga.dev>
2024-12-08 17:04:16 +0900

    test: クエリダイエット

--- go/owner_handlers.go
+++ go/owner_handlers.go
@@ -195,26 +195,16 @@ func ownerGetChairs(w http.ResponseWriter, r *http.Request) {
 	owner := ctx.Value("owner").(*Owner)
 
 	chairs := []chairWithDetail{}
-	if err := db.SelectContext(ctx, &chairs, `SELECT id,
-       owner_id,
+	if err := db.SelectContext(ctx, &chairs, `
+SELECT id,
        name,
-       access_token,
        model,
        is_active,
        created_at,
-       updated_at,
-       IFNULL(total_distance, 0) AS total_distance,
-       total_distance_updated_at
+       IFNULL(distance_cache.total_distance, 0) AS total_distance,
+       distance_cache.total_distance_updated_at
 FROM chairs
-       LEFT JOIN (SELECT chair_id,
-                          SUM(IFNULL(distance, 0)) AS total_distance,
-                          MAX(created_at)          AS total_distance_updated_at
-                   FROM (SELECT chair_id,
-                                created_at,
-                                ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                         FROM chair_locations) tmp
-                   GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
+LEFT JOIN chair_distance_cache distance_cache ON distance_cache.chair_id = chairs.id
 WHERE owner_id = ?
 `, owner.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
@@ -222,9 +212,9 @@ WHERE owner_id = ?
 	}
 
 	res := ownerGetChairResponse{
-		Chairs: make([]ownerGetChairResponseChair, 0, len(chairs)),
+		Chairs: make([]ownerGetChairResponseChair, len(chairs)),
 	}
-	for _, chair := range chairs {
+	for i, chair := range chairs {
 		c := ownerGetChairResponseChair{
 			ID:            chair.ID,
 			Name:          chair.Name,
@@ -237,7 +227,7 @@ WHERE owner_id = ?
 			t := chair.TotalDistanceUpdatedAt.Time.UnixMilli()
 			c.TotalDistanceUpdatedAt = &t
 		}
-		res.Chairs = append(res.Chairs, c)
+		res.Chairs[i] = c
 	}
 	writeJSON(w, http.StatusOK, res)
 }
--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -147,3 +147,24 @@ CREATE TABLE coupons
   COMMENT 'クーポンテーブル';
 
 CREATE INDEX idx_coupons_usedby ON coupons(used_by);
+
+
+CREATE TABLE chair_distance_cache (
+    chair_id VARCHAR(255) PRIMARY KEY,
+    total_distance INT NOT NULL,
+    total_distance_updated_at DATETIME NOT NULL
+);
+
+INSERT INTO chair_distance_cache (chair_id, total_distance, total_distance_updated_at)
+SELECT chair_id,
+       SUM(IFNULL(distance, 0)) AS total_distance,
+       MAX(created_at)          AS total_distance_updated_at
+FROM (SELECT chair_id,
+             created_at,
+             ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
+             ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
+      FROM chair_locations) tmp
+GROUP BY chair_id
+ON DUPLICATE KEY UPDATE
+    total_distance = VALUES(total_distance),
+    total_distance_updated_at = VALUES(total_distance_updated_at);
