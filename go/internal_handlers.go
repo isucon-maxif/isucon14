538c538cfaf455005ce56c69635f188c5172ab52
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-12-08 12:08:14 +0900

    feat: internal handler のマッチング処理を近い順に greedy にする

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -9,9 +9,10 @@ import (
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
+
+	// 待っているリクエストを取得
+	rides := []*Ride{}
+	if err := db.SelectContext(ctx, &rides, "SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at"); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			w.WriteHeader(http.StatusNoContent)
 			return
@@ -20,38 +21,72 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
+	// 空きイスとその座標を取得
+	freeChairs := []*Chair{}
+	if err := db.SelectContext(ctx, &freeChairs, "SELECT * FROM chairs WHERE is_active = TRUE AND NOT EXISTS (SELECT * FROM ride_statuses WHERE EXISTS (SELECT * FROM rides WHERE rides.chair_id = chairs.id AND rides.id = ride_statuses.ride_id AND status != 'COMPLETED'))"); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	chairByAuthTokenCacheMutex.Lock()
-	delete(chairByAuthTokenCache, matched.AccessToken)
-	chairByAuthTokenCacheMutex.Unlock()
+	// イスの座標を取得
+	tmp := []*ChairLocation{}
+	if err := db.SelectContext(ctx, &tmp, "SELECT A.chair_id, A.latitude, A.longitude FROM chair_locations A INNER JOIN (SELECT chair_id, MAX(created_at) AS cat FROM chair_locations GROUP BY chair_id) B ON A.chair_id = B.chair_id AND A.created_at = B.cat"); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+	chairLocations := map[string]*ChairLocation{}
+	for _, loc := range tmp {
+		chairLocations[loc.ChairID] = loc
+	}
+
+	// イスの性能を取得
+	tmp2 := []*ChairModel{}
+	if err := db.SelectContext(ctx, &tmp2, "SELECT * FROM chair_models"); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+	chairModels := map[string]int{}
+	for _, model := range tmp2 {
+		chairModels[model.Name] = model.Speed
+	}
+
+	// マッチング
+	isChairUsed := map[int]bool{}
+	for _, ride := range rides {
+		bestChairIdx := -1
+		bestChairTime := 1000000000.0
+		for i, chair := range freeChairs {
+			if isChairUsed[i] {
+				continue
+			}
+			loc, ok := chairLocations[chair.ID]
+			if !ok {
+				continue
+			}
+			dist := abs(loc.Latitude-ride.PickupLatitude) + abs(loc.Longitude-ride.PickupLongitude)
+			speed := chairModels[chair.Model]
+			time := float64(dist) / float64(speed)
+			if time < bestChairTime {
+				bestChairTime = time
+				bestChairIdx = i
+			}
+		}
+		if bestChairIdx == -1 {
+			continue
+		}
+		isChairUsed[bestChairIdx] = true
+		if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", freeChairs[bestChairIdx].ID, ride.ID); err != nil {
+			writeError(w, http.StatusInternalServerError, err)
+			return
+		}
+		chairByAuthTokenCacheMutex.Lock()
+		delete(chairByAuthTokenCache, freeChairs[bestChairIdx].AccessToken)
+		chairByAuthTokenCacheMutex.Unlock()
+	}
 
 	w.WriteHeader(http.StatusNoContent)
 }
