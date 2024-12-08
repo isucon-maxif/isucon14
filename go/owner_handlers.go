0616e4360f5efadca2f24c0958f76aaa945a157e
Ryoga-exe <git@ryoga.dev>
2024-12-08 12:55:56 +0900

    test: クエリ改善

--- go/owner_handlers.go
+++ go/owner_handlers.go
@@ -195,28 +195,30 @@ func ownerGetChairs(w http.ResponseWriter, r *http.Request) {
 	owner := ctx.Value("owner").(*Owner)
 
 	chairs := []chairWithDetail{}
-	if err := db.SelectContext(ctx, &chairs, `SELECT id,
-       owner_id,
-       name,
-       access_token,
-       model,
-       is_active,
-       created_at,
-       updated_at,
-       IFNULL(total_distance, 0) AS total_distance,
-       total_distance_updated_at
-FROM chairs
-       LEFT JOIN (SELECT chair_id,
-                          SUM(IFNULL(distance, 0)) AS total_distance,
-                          MAX(created_at)          AS total_distance_updated_at
-                   FROM (SELECT chair_id,
-                                created_at,
-                                ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                         FROM chair_locations) tmp
-                   GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
-WHERE owner_id = ?
-`, owner.ID); err != nil {
+
+	if err := db.SelectContext(ctx, &chairs, `SELECT c.id,
+        c.owner_id,
+        c.name,
+        c.access_token,
+        c.model,
+        c.is_active,
+        c.created_at,
+        c.updated_at,
+        COALESCE(cl.total_distance, 0) as total_distance,
+        cl.created_at as total_distance_updated_at
+    FROM chairs c
+    LEFT JOIN (
+        SELECT chair_id, 
+               total_distance,
+               created_at
+        FROM chair_locations
+        WHERE (chair_id, created_at) IN (
+            SELECT chair_id, MAX(created_at)
+            FROM chair_locations
+            GROUP BY chair_id
+        )
+    ) cl ON cl.chair_id = c.id
+    WHERE c.owner_id = ?`, owner.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
